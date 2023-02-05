539189ceb779994df5aecb6f8b4347aef793e42d
ThEWoLf882-42 <agimi@student.1337.ma>
2023-02-05 18:24:39 +0100

    fixed

--- sources/pipex.c
+++ sources/pipex.c
@@ -6,7 +6,7 @@
 /*   By: agimi <agimi@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 12:28:23 by agimi             #+#    #+#             */
-/*   Updated: 2023/02/05 17:46:08 by agimi            ###   ########.fr       */
+/*   Updated: 2023/02/05 18:15:59 by agimi            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int	main(int argc, char **argv, char **env)
 	else
 	{
 		ft_putstr_fd("\033[31mError: Bad arguments\n\e[0m", 2);
-		ft_putstr_fd("synstax: ./pipex <file1> <cmd1> <cmd2> <file2>\n", 2);
+		ft_putstr_fd("pipex: ./pipex <file1> <cmd1> <cmd2> <file2>\n", 2);
 		exit(127);
 	}
 	return (0);
